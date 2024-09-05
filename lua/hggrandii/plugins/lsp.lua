c59c7d15a15a80882894a95c0c101922ea5d1e80
Weekend <helgigudjonthordarson@yahoo.com>
2024-09-05 18:30:45 +0000

    lsp update dropped ts for now

--- lua/hggrandii/plugins/lsp.lua
+++ lua/hggrandii/plugins/lsp.lua
@@ -27,7 +27,10 @@ return {
 			cmp_lsp.default_capabilities()
 		)
 
+		-- Fidget (LSP progress UI)
 		require("fidget").setup({})
+
+		-- Mason setup for managing LSP servers
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = {
@@ -36,130 +39,77 @@ return {
 				"gopls",
 				"pyright",
 			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							local buf_set_keymap = vim.api.nvim_buf_set_keymap
-							local opts = { noremap = true, silent = true }
+		})
 
-							-- Key mappings
-							buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-							buf_set_keymap(
-								bufnr,
-								"n",
-								"<space>wa",
-								"<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",
-								opts
-							)
-							buf_set_keymap(
-								bufnr,
-								"n",
-								"<space>wr",
-								"<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",
-								opts
-							)
-							buf_set_keymap(
-								bufnr,
-								"n",
-								"<space>wl",
-								"<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-								opts
-							)
-							buf_set_keymap(bufnr, "n", "<space>D", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<space>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<leader>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-							buf_set_keymap(bufnr, "n", ">", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<space>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<space>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-							buf_set_keymap(bufnr, "n", "<leader>vca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+		local on_attach = function(client, bufnr)
+			local buf_set_keymap = vim.api.nvim_buf_set_keymap
+			local opts = { noremap = true, silent = true }
 
-							-- Debugging: Print buffer number and client name
-							print("LSP attached to buffer", bufnr, "with client", client.name)
-						end,
-					})
-				end,
+			buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<space>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<space>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+			buf_set_keymap(
+				bufnr,
+				"n",
+				"<space>wl",
+				"<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+				opts
+			)
+			buf_set_keymap(bufnr, "n", "<space>D", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<space>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<leader>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+			buf_set_keymap(bufnr, "n", ">", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<space>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<space>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+			buf_set_keymap(bufnr, "n", "<leader>vca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+		end
 
-				zls = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.zls.setup({
-						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-						settings = {
-							zls = {
-								enable_inlay_hints = true,
-								enable_snippets = true,
-								warn_style = true,
-							},
-						},
-					})
-					-- Ensure these settings are correctly set
-					vim.g.zig_fmt_parse_errors = 0
-					vim.g.zig_fmt_autosave = 0
-				end,
-
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-								},
-							},
-						},
-					})
-				end,
+		-- Configure individual language servers
+		require("lspconfig").lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					runtime = { version = "LuaJIT" },
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+				},
 			},
 		})
 
-		-- Setup Flutter Tools
+		require("lspconfig").rust_analyzer.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		require("lspconfig").gopls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		require("lspconfig").pyright.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		-- Setup for Flutter tools
 		require("flutter-tools").setup({
 			lsp = {
 				capabilities = capabilities,
-				on_attach = function(client, bufnr)
-					local buf_set_keymap = vim.api.nvim_buf_set_keymap
-					local opts = { noremap = true, silent = true }
-
-					-- Key mappings for Dart/Flutter
-					buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-					buf_set_keymap(
-						bufnr,
-						"n",
-						"<space>wl",
-						"<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-						opts
-					)
-					buf_set_keymap(bufnr, "n", "<space>D", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<æ", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<ð", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<space>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-					buf_set_keymap(bufnr, "n", "<leader>vca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-
-					-- Debugging: Print buffer number and client name
-					print("LSP attached to buffer", bufnr, "with client", client.name)
-				end,
+				on_attach = on_attach,
 			},
 		})
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
+		-- Completion setup
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -167,8 +117,8 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = cmp.mapping.complete(),
 			}),
@@ -180,8 +130,10 @@ return {
 			}),
 		})
 
+		-- Trouble.nvim for LSP diagnostics and navigation
 		require("trouble").setup({})
 
+		-- Diagnostic configuration
 		vim.diagnostic.config({
 			float = {
 				focusable = false,
